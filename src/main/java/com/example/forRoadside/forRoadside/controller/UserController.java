93d85b87cae55ea5905503509bcc1cec553c7a20
Kibria92 <kibria92@gmail.com>
2020-04-26 02:18:56 +0600

    add return server response 2

--- src/main/java/com/example/forRoadside/forRoadside/controller/UserController.java
+++ src/main/java/com/example/forRoadside/forRoadside/controller/UserController.java
@@ -111,7 +111,7 @@ public class UserController {
     @DeleteMapping("/deleteUser/{id}")//useing return server responce
     public ResponseEntity<User> deleteUser(@PathVariable int id) {
         Optional<User> userHave = userService.getUser(id);
-        if (userHave != null) {
+        if (userHave != null) {//if there is any user by the id
             userService.deleteUser(id);
             return new ResponseEntity<User>(HttpStatus.OK);
         } else {
